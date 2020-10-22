49305585c7d482a1306258a1fa7bf508ff0e207d
ReyaadElkomy <reyaadelkomy@hotmail.com>
2020-10-22 18:56:09 +0200

    Create Add, Delete from Array with array functions.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ var names = [
     "Reyaad", 
     "Mohamed", 
     "Ali", 
-    "Elomy", 
+    "Elomy",
+    "Tammer", 
+    "Hassan" 
 ]
 
 if(Array.isArray(names)){
@@ -10,3 +12,24 @@ if(Array.isArray(names)){
 }else{
     console.log("Sorry, This is not array");
 }
+
+// Delete the first element in array 
+console.log(names);
+var firstElement = names.shift();
+console.log(firstElement);
+
+// Get the last element in array 
+console.log(names);
+var lastElment = names.pop();
+console.log(lastElment);
+
+console.log(names);
+console.log(names.length); // lenght
+
+// names.splice(2, 2); delete from 
+names.pop(); // Delete the last element in array
+console.log(names.length);
+console.log(names);
+names.push("Yassmen"); // Add new value to the last ement in array.
+console.log(names.length);
+console.log(names);
