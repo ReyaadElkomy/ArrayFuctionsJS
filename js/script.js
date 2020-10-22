c7aea6a2dd2eb46e34ffab64d195e6bbc4a10008
ReyaadElkomy <reyaadelkomy@hotmail.com>
2020-10-22 13:57:35 +0200

    First summary
    First summary description

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var names = [
+    "Reyaad", 
+    "Mohamed", 
+    "Ali", 
+    "Elomy", 
+]
+
+if(Array.isArray(names)){
+    console.log("Good, this is araay");
+}else{
+    console.log("Sorry, This is not array");
+}
